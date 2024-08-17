1ced4852ea0c8d66943fa31f14dc9ce569747778
Avalokita Sharma <avalokitasharma@Avalokitas-MacBook-Pro.local>
2024-08-17 22:17:55 +0530

    run migrations

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,15 +4,25 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/avalokitasharma/transaction_service/transaction_service/internal/api"
 	"github.com/avalokitasharma/transaction_service/transaction_service/internal/repository"
 	"github.com/avalokitasharma/transaction_service/transaction_service/internal/service"
+	"github.com/avalokitasharma/transaction_service/transaction_service/pkg/database"
 	_ "github.com/lib/pq"
 )
 
 func main() {
 	db, err := sql.Open("postgres", "postgres://username:password@localhost/dbname?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	migrationsPath, err := filepath.Abs("./migrations")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = database.RunMigrations(db, migrationsPath)
 	if err != nil {
 		log.Fatal(err)
 	}
